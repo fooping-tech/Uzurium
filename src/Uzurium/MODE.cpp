5290704db17df3e46db2360b781037fe636e8b3d
Fooping <fukuhala@gmail.com>
2023-11-14 20:37:33 +0900

    add Mode

--- src/Uzurium/MODE.cpp
+++ src/Uzurium/MODE.cpp
@@ -2,7 +2,7 @@
 
 
 uint32_t MODE::CheckSpentTime(){
-  return 0;
+  return spentTime;
 }
 void MODE::mainloop(){
   //前回実行時からの経過時間を計算
@@ -10,13 +10,84 @@ void MODE::mainloop(){
   //経過時間を計算
   spentTime = millis() - startTime;
 
+
   if(deltaTime >= TaskSpan){
+    if(!_InitCondition){
+      InitFunction();
+    }else{
       main();
-      //cycleTimeリセット
-      cycleTime = millis();
+    }
+    //cycleTimeリセット
+    cycleTime = millis();
   }
 }
-  
+
+void MODE::InitFunction(){
+  motor->move(10);
+  led->counter(_InitCounter);
+  _InitCounter++;
+  if(_InitMode==0){
+    if(spentTime>0)motor->changeFreq(8);
+    if(spentTime>100)motor->changeFreq(12);
+    if(spentTime>200)motor->changeFreq(11);
+    if(spentTime>300)motor->changeFreq(10);
+    if(spentTime>400)motor->changeFreq(9);
+    if(spentTime>500)motor->changeFreq(8);
+    if(spentTime>600){
+      motor->move(0);
+      _InitCondition =true;
+    }
+  }
+
+  if(_InitMode==1){
+    if(spentTime>0)motor->changeFreq(8);
+    if(spentTime>100)motor->changeFreq(9);
+    if(spentTime>200)motor->changeFreq(10);
+    if(spentTime>300)motor->changeFreq(11);
+    if(spentTime>400)motor->changeFreq(12);
+    if(spentTime>500)motor->changeFreq(8);
+    if(spentTime>600){
+      motor->move(0);
+      _InitCondition =true;
+    }
+  }
+  if(_InitMode==2){
+    if(spentTime>0)motor->changeFreq(8);
+    if(spentTime>100)motor->changeFreq(12);
+    if(spentTime>200)motor->changeFreq(9);
+    if(spentTime>300)motor->changeFreq(12);
+    if(spentTime>400)motor->changeFreq(9);
+    if(spentTime>500)motor->changeFreq(12);
+    if(spentTime>600)motor->changeFreq(9);
+    if(spentTime>700)motor->changeFreq(12);
+    if(spentTime>800)motor->changeFreq(8);
+    if(spentTime>900){
+      motor->move(0);
+      _InitCondition =true;
+    }
+  }
+  if(_InitMode==3){
+    if(spentTime>0)motor->changeFreq(8);
+    if(spentTime>100)motor->changeFreq(9);
+    if(spentTime>200){
+      motor->changeFreq(10);
+    }
+    if(spentTime>300){
+      motor->move(0);
+    }
+    if(spentTime>400){
+      motor->move(10);
+      motor->changeFreq(11);
+    }
+    if(spentTime>500)motor->changeFreq(12);
+    if(spentTime>600)motor->changeFreq(13);
+    if(spentTime>700){
+      motor->move(0);
+      _InitCondition =true;
+    }
+  }
+}
 void MODE::main(){
       
 }
+
--- src/Uzurium/MODE.h
+++ src/Uzurium/MODE.h
@@ -8,22 +8,29 @@
 
 class MODE{
   public:
-    MODE(){//コンストラクタ
+    MODE(PHOTO *p,DCMPWM *m,RINGLED *l){//コンストラクタ
       TaskSpan=100;
       startTime = millis();
       cycleTime = millis();
       adv=0;
       active=false;
-    } 
+      str="";
+      photo = p;
+      motor = m;
+      led = l;
+    }
     ~MODE(){//デストラクタ
     }
     uint32_t CheckSpentTime();
     void mainloop();
     virtual void main();
     bool active=false;
+    char* str;
     void GetAdValue(int value){
       adv = value;
     }
+    void InitFunction();
+    
   protected:
     int TaskSpan;
     uint32_t startTime;
@@ -31,25 +38,49 @@ class MODE{
     uint32_t deltaTime;
     uint32_t spentTime;
     int adv;
-
+    bool _InitCondition=false;
+    int _InitCounter=0;
+    int _InitMode=0;
+    DCMPWM *motor;
+    PHOTO *photo;
+    RINGLED *led;
 };
 
+/*
+class BuzzerMode : public MODE {
+public:
+    BuzzerMode(int m) : MODE() {
+     Serial.println("<----BuzzerMode_begin---->");
+     active=true;
+     seq=m;
+    }
+    // main関数をオーバーライド
+    void main() override {
+        motor->move(10);
+        led->counter(photo->CheckCounter());
+    }
 
+    private:
+        int seq=0;
+        
+};
+*/
 class ADinputMode : public MODE {
 public:
-    ADinputMode(PHOTO *p,DCMPWM *m,RINGLED *l) : MODE() {
+    ADinputMode(PHOTO *p,DCMPWM *m,RINGLED *l) : MODE(p,m,l) {
      Serial.println("<----ADinputMode_begin---->");
      photo = p;
      motor = m;
      led = l;
      active=true;
+     _InitMode=2;
     }
+  
     // main関数をオーバーライド
     void main() override {
       // カスタムモードの処理を実装
       //RPMを計測する
       photo->CalcNowRPM();
-      
       //シリアル通信のデータを書き出す
       //SERIAL_SetSerialData();
       //エッジがしばらく来ない場合にRPM初期化
@@ -57,15 +88,25 @@ public:
       //PID制御によりDUTYを計算する
       photo->ClacDuty(deltaTime);
       //BUZZERを鳴らしていない時
-//      if(!BUZZER_CheckInit()){
-        //算出したDUTYでモータを回す
-        //motor.move(PHOTO_CheckDuty());
-        //ポテンショ対応;DUTYを上書き
-        int duty = map(adv,0,4095,0,200);
-        motor->move(duty);
-        //DUTYに応じてLEDのhueを可変
-        led->fire2(4,duty+100);
-//      }
+      //算出したDUTYでモータを回す
+
+      //ポテンショ対応;DUTYを上書き
+      int duty = map(adv,0,4095,0,200);
+      photo->SetDuty(duty);
+      motor->move(photo->CheckDuty());
+      //motor->move(duty);
+      //DUTYに応じてLEDのhueを可変
+      led->fire2(4,photo->CheckDuty()+100);
+
+      //Serial.print(spentTime);
+      //Serial.print(",");
+      Serial.print(photo->CheckRpm());
+      Serial.print(",");
+      Serial.print(photo->CheckNowRPM());
+      Serial.print(",");
+      Serial.print(photo->CheckTargetRPM());
+      Serial.print(",");
+      Serial.println(photo->CheckDuty());
 
     }
 
@@ -78,12 +119,13 @@ public:
   };
 class StopMode : public MODE {
 public:
-    StopMode(PHOTO *p,DCMPWM *m,RINGLED *l) : MODE() {
+    StopMode(PHOTO *p,DCMPWM *m,RINGLED *l) : MODE(p,m,l) {
      Serial.println("<----StopMode_begin---->");
      photo = p;
      motor = m;
      led = l;
      active=false;
+     _InitMode=0;//初期化時のブザーモード
     }
     // main関数をオーバーライド
     void main() override {
@@ -95,10 +137,120 @@ public:
     }
 
     private:
-      DCMPWM *motor;
-      PHOTO *photo;
-      RINGLED *led;
-      
-        
   };
+
+class FeedBackMode : public MODE {
+public:
+    FeedBackMode(PHOTO *p,DCMPWM *m,RINGLED *l) : MODE(p,m,l) {
+     Serial.println("<----FeedBackMode_begin---->");
+     photo = p;
+     motor = m;
+     led = l;
+     active=true;
+     startTime = millis();
+     _InitMode=1;
+    }
+    // main関数をオーバーライド
+    void main() override {
+//RPMを計測する
+      photo->CalcNowRPM();
+      //シリアル通信のデータを書き出す
+      //SERIAL_SetSerialData();
+      //エッジがしばらく来ない場合にRPM初期化
+      photo->CheckTimeout();
+      //BUZZERを鳴らしていない時
+//      if(!BUZZER_CheckInit()){
+         //PID制御によりDUTYを計算する
+        photo->ClacDuty(deltaTime);
+        //算出したDUTYでモータを回す
+        motor->move(photo->CheckDuty());
+        //DUTYに応じてLEDのhueを可変
+        led->fire2(4,photo->CheckDuty());
+//      }
+      //Kpをポテンショで可変
+      float Kp = map(adv,0,4095,0,1000) * 0.000001;
+//      Serial.print("Kp=");
+//      Serial.println(Kp,8);
+      photo->SetKp(Kp);
+
+
+      Serial.print(photo->CheckRpm());
+      Serial.print(",");
+      Serial.print(photo->CheckNowRPM());
+      Serial.print(",");
+      Serial.print(photo->CheckTargetRPM());
+      Serial.print(",");
+      Serial.println(photo->CheckDuty());
+
+      //脱調判定
+      photo->CheckOutOfStep();
+      //TargetRPM設定
+      if(spentTime>1000)photo->SetTargetRPM(0);
+      if(spentTime>3000) photo->SetTargetRPM(1000);
+      if(spentTime>10000)photo->SetTargetRPM(1500);
+      if(spentTime>15000)photo->SetTargetRPM(2000);
+      if(spentTime>20000)photo->SetTargetRPM(2500);
+      if(spentTime>25000)photo->SetTargetRPM(3000);
+      if(spentTime>30000)photo->SetTargetRPM(3000);
+      if(spentTime>35000)photo->SetTargetRPM(2500);
+      if(spentTime>40000)photo->SetTargetRPM(1500);
+      if(spentTime>45000)photo->SetTargetRPM(0);
+
+      //脱調していたら
+      if(photo->CheckOutFlag()){
+      }
+
+    }
+
+    private:
+
+};
+
+class PhotoReflectorInspectionMode : public MODE {
+public:
+    PhotoReflectorInspectionMode(PHOTO *p,DCMPWM *m,RINGLED *l) : MODE(p,m,l) {
+     Serial.println("<----PhotoReflectorInspectionMode_begin---->");
+     photo = p;
+     motor = m;
+     led = l;
+     active=true;
+     _InitMode=3;
+    }
+    // main関数をオーバーライド
+    void main() override {
+        motor->move(0);
+        led->counter(photo->CheckCounter());
+    }
+
+    private:
+
+};
+
+class LedInspectionMode : public MODE {
+public:
+    LedInspectionMode(PHOTO *p,DCMPWM *m,RINGLED *l) : MODE(p,m,l) {
+     Serial.println("<----LedInspectionMode_begin---->");
+     photo = p;
+     motor = m;
+     led = l;
+     active=true;
+    }
+    // main関数をオーバーライド
+    void main() override {
+        motor->move(0);
+        if(2000>=spentTime && spentTime>0)led->inspection(2);
+        if(4000>=spentTime && spentTime>2000)led->inspection(0);
+        if(6000>=spentTime && spentTime>4000)led->inspection(1);
+        if(8000>=spentTime && spentTime>6000)led->inspection(2);
+        if(10000>=spentTime && spentTime>8000)led->inspection(0);
+        if(12000>=spentTime && spentTime>10000)led->inspection(1);
+        if(14000>=spentTime && spentTime>12000)led->inspection(2);
+        if(16000>=spentTime && spentTime>14000)led->inspection(0);
+        if(18000>=spentTime && spentTime>16000)led->inspection(1);
+        if(spentTime>18000)led->inspection(2);
+    }
+
+    private:
+
+};
 #endif

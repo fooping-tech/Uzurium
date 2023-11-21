511676f3e72a4f7d4b3ff7d43586caf1878e078e
Fooping <fukuhala@gmail.com>
2023-11-22 01:03:34 +0900

    Update configuration.h

--- src/Uzurium/configuration.h
+++ src/Uzurium/configuration.h
@@ -1,58 +1,38 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
-//Debug
-#define ARDUINOTRACE_ENABLE 1  // 0:Disable all traces(TRACE,DUMP)
-#include <ArduinoTrace.h>
-
 #include <M5Unified.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Wire.h>
+
 #include "DCMPWM.h"
 #include "RINGLED.h"
 #include "PHOTO.h"
 #include "MODE.h"
 #include "SW.h"
 
-#include <esp_now.h>
-#include <WiFi.h>
-
-//INA219
-#include <Wire.h>
-#include <Adafruit_INA219.h>
-Adafruit_INA219 ina219;
-
-
 //motor pin setting
-#define MOTOR_PIN 23 //ATOM:32
-//#define MOTOR_PIN 33 //STAMP:33
+#define MOTOR_PIN 23
 
-//UzuriumNumber
-//#define NUMBER 1
-
-//PWM CH(for MotorDrive)
+//motor PWM CH(for MotorDrive)
 #define CHANNEL 2
 
-#define LED_PIN 22 // M5Atom:26
-//#define LED_PIN 32 //M5Stamp:32
+//led pin setting
+#define LED_PIN 22
 
-//PhotoSensor
+//PhotoSensor pin setting
 #define PHOTO_PIN 19
 
-//Mic
+//Analog pin setting
 #define ANALOG_PIN 33
 
-//SW
+//SW setting モーメンタリ
 #define SW_PIN 25
-//SW
-#define TEST_SW_PIN 21
-//SW
-#define BTN_SW_PIN 39
 
-//PID定数
-float Kp = 0.0007;
-const float Ki = 0;
-const float Kd = 0;
-//LED Brightness
-int LedBrightness =75;
-int LedBrightnessLow =10;
+//SW setting オルタネート
+#define TEST_SW_PIN 21
+
+int Uzurium_Number = 0;
 
 #endif
